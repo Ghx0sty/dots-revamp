2808ef065baaccaa703f75c48d99b25bbed4d040
Ghx0sty <blobman320@gmail.com>
2025-05-24 23:53:39 +0100

    Changed my hostname

--- flake.nix
+++ flake.nix
@@ -54,7 +54,7 @@
     # Available through 'nixos-rebuild --flake .#your-hostname'
     nixosConfigurations = {
       # FIXME replace with your hostname
-      your-hostname = nixpkgs.lib.nixosSystem {
+      hackpc = nixpkgs.lib.nixosSystem {
         specialArgs = {inherit inputs outputs;};
         modules = [
           # > Our main nixos configuration file <
