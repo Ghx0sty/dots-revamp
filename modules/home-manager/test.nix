21ed9172ef5b98c3f8516f391a82f7a8e852ac0e
Ghx0sty <blobman320@gmail.com>
2025-05-30 03:37:01 +0100

    Test.nix file

---
+++ modules/home-manager/test.nix
@@ -0,0 +1,15 @@
+{ config, lib, pkgs, ... }:
+
+{
+  home.activation.githubSync = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
+    export PATH=${pkgs.git}/bin:/run/wrapper/bin:$PATH
+    # userdir=$(getent passwd 1000 | cut -d: -f6)
+    dotsdir="$HOME/.nixdots"
+
+    echo "Syncing dots to GitHub..."
+    cd $dotsdir
+    git add *
+    git commit -m "Testing automation"
+    git push
+  '';
+}
