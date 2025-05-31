568fe5914a9a853b4513553e9d873864d8551f2c
Ghx0sty <blobman320@gmail.com>
2025-05-31 23:21:01 +0100

    Adapting nixos module and admitting defeat for now

--- modules/nixos/pull-sync.nix
+++ modules/nixos/pull-sync.nix
@@ -3,10 +3,13 @@
 {
   system.activationScripts.pullSync = {
     text = ''
+    # TODO: This script fetches the first user you make by UID (1000). Make sure this is the one you want running stuff!
+    # Also note, this script makes your dots push ssh based!
     export PATH=${pkgs.git}/bin:$PATH
 
     # FIXME: Fill this out with your own link!
     githublink="https://github.com/Ghx0sty/dots-revamp.git"
+    githubssh="git@github.com:Ghx0sty/dots-revamp.git"
 
     user=$(getent passwd 1000 | cut -d: -f1)
     userdir=$(getent passwd 1000 | cut -d: -f6)
@@ -16,6 +19,7 @@
       echo "First time setup; cloning repo and putting it in the right places"
       mkdir $dotsdir
       git clone $githublink $dotsdir
+      git remote set-url --push origin $githubssh
       chown -R $user:users $dotsdir
       rm -rf $nixosdir
       ln -s $dotsdir $nixosdir
