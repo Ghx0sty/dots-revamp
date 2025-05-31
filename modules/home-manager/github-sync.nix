efcaf79c5c51018a36c1dd289a29a2082ad26902
Ghx0sty <blobman320@gmail.com>
2025-05-31 12:44:39 +0100

    Why?

--- modules/home-manager/github-sync.nix
+++ modules/home-manager/github-sync.nix
@@ -4,6 +4,7 @@
   home.activation.githubSync = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
     export PATH=${pkgs.git}/bin:${pkgs.openssh}/bin:/run/wrapper/bin:$PATH
     dotsdir="$HOME/.nixdots"
+    ssh -T git@github.com -o BatchMode=yes
 
     authcheck=$(ssh -T git@github.com -o BatchMode=yes 2>&1)
 
