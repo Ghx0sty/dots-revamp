b3b037b4b1126e23a811eec168865d8ca28bdf88
Ghx0sty <blobman320@gmail.com>
2025-05-31 12:41:07 +0100

    Testing further

--- modules/home-manager/test.nix
+++ modules/home-manager/test.nix
@@ -2,15 +2,16 @@
 
 {
   home.activation.githubSync = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    dotsdir="$HOME"
+    homedir="$HOME"
     user="$USER"
 
     echo "Trying out some fun stuff!"
     touch $dotsdir/hello.txt
-    echo "My dir: $dotsdir" >> $dotsdir/hello.txt
+    echo "My dir: $homedir" >> $dotsdir/hello.txt
     echo "My user: $user" >> $dotsdir/hello.txt
     echo "Try to check out hello.txt"
     id >> $dotsdir/hello.txt
     echo "My PATH: $PATH" >> $dotsdir/hello.txt
+    echo "And what's $dotsdir"
   '';
 }
