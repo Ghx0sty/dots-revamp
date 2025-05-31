177b34cfd56254d8a41266c3a7fd39c0fc3d1225
Ghx0sty <blobman320@gmail.com>
2025-05-31 20:36:46 +0100

    Trying new github-sync design

--- modules/home-manager/github-sync.nix
+++ modules/home-manager/github-sync.nix
@@ -19,33 +19,39 @@
       base=$(git merge-base @ @{u})
       dirty=$(git status --porcelain)
 
-      if [[ $local == $remote && -z $dirty ]]; then
+      if [[ -z $dirty ]]; then
+        echo "Tree is not dirty, no commit made"
+      else
+        echo "Dirty tree, creating new commit"
+        git add .
+        git commit -m "Automated push"
+      fi
+
+      if [[ $local == $remote ]]; then
         echo "All up to date, skipping"
         exit 0
       elif [[ $local == $base ]]; then
         echo "Out of date! Pulling latest changes"
         git config pull.rebase false
         git pull
-        if [[ -z $dirty ]]; then
-          echo "Looks like you have no more changes to apply. Updated local successfully"
-          exit 0
-        fi
+        # if [[ -z $dirty ]]; then
+          # echo "Looks like you have no more changes to apply. Updated local successfully"
+          # exit 0
+        # fi
       elif [[ $remote == $base ]]; then
-        if [[ -z dirty ]]; then
-          echo "It looks like you already got a commit, I'll just push it for you"
-          git push
-          exit 0
-        fi
+        echo "All clear, pushing newest change"
+        # if [[ -z dirty ]]; then
+          # echo "It looks like you already got a commit, I'll just push it for you"
+          # git push
+          # exit 0
+        # fi
       else
         echo "What happened? Commits diverged somehow. Fix that yourself, you're mucking about"
         exit 0
       fi
 
       # Note for later: figure out what's happening with the logic at -n $dirty
-      echo "Making fresh commit and pushing onto repo"
 
-      git add .
-      git commit -m "Automated push"
       git push
     else
       echo "You aren't authenticated to Github yet!"
