b743f5442b589044e3bcab7a9bb8befa275019e8
Ghx0sty <blobman320@gmail.com>
2025-06-09 12:56:24 +0000

    Automated push

--- home-manager/base/home.nix
+++ home-manager/base/home.nix
@@ -105,8 +105,8 @@
         "$secondmod, up, movewindow, u"
         "$secondmod, down, movewindow, d"
 
-        "CTRL+$mainmod, right, workspace, e+1"
-        "CTRL+$mainmod, left, workspace, e-1"
+        "CTRL+ALT, right, workspace, e+1"
+        "CTRL+ALT, left, workspace, e-1"
       ];
     };
   };
