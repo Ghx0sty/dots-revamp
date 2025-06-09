cfb59296c450f057aad62f201dd33f7f716b3c7d
Ghx0sty <blobman320@gmail.com>
2025-06-09 12:51:10 +0000

    Automated push

--- home-manager/base/home.nix
+++ home-manager/base/home.nix
@@ -84,7 +84,7 @@
       ];
       bind = [
         "$mainmod, q, exec, $terminal"
-        "$secondmod, a, exec, $launcher-kill || $launcher-start"
+        "$secondmod, a, exec, killall wofi || $launcher-start"
         "$secondmod, b, exec, $browser"
         "$mainmod, c, killactive"
         "$mainmod, m, exit"
