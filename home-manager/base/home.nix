850565c79d2ba4ef86bd1f3399425be9e54ad28e
Ghx0sty <blobman320@gmail.com>
2025-06-09 13:26:28 +0000

    Automated push

--- home-manager/base/home.nix
+++ home-manager/base/home.nix
@@ -73,7 +73,7 @@
     enable = true;
     settings = {
       "$terminal" = "alacritty";
-      "$launcher-kill" = "killall wofi"; 
+      "$launcher-kill" = "pkill -x wofi"; 
       "$launcher-start" = "wofi --show drun --gtk-dark --allow-images";
       "$browser" = "librewolf";
       
