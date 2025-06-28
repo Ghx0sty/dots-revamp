48aff95d15b3a9b655e0abc33621e68338571f45
zenex458 <emajeed458@gmail.com>
2025-06-28 13:22:11 +0100

    Refactored home.nix to group everything together and moved hyprland config to a seperate file, as it can get big

--- home-manager/base/home.nix
+++ home-manager/base/home.nix
@@ -21,6 +21,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
+    ./hypr.nix
   ];
 
   # Nixpkgs configuration moved to flake.nix
@@ -29,107 +30,64 @@
   home = {
     username = "matt";
     homeDirectory = "/home/matt";
+    stateVersion = "25.05";
+    packages = with pkgs; [
+      neofetch
+      lunarvim
+      keychain
+    ];
   };
 
   # Add stuff for your user as you see fit:
   # programs.neovim.enable = true;
-  home.packages = with pkgs; [ 
-    neofetch
-    lunarvim
-    keychain
-  ];
 
   # Enable home-manager and git
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
 
-  programs.git = {
-    enable = true;
-    userName = "Ghx0sty";
-    userEmail = "blobman320@gmail.com";
-  };
-
-  programs.ssh = {
-    enable = true;
-    addKeysToAgent = "yes";
-  };
-
-  programs.zsh = {
-    enable = true;
-    shellAliases = {
-      home-sync = "home-manager switch --flake ~/.nixdots#matt@hackpc";
-      nixos-switch = "sudo nixos-rebuild switch --flake ~/.nixdots#hackpc";
-      nixos-sync = "sudo nixos-rebuild switch --flake ~/.nixdots#hackpc && home-manager switch --flake ~/.nixdots#matt@hackpc";
-      flake-update = "nix flake update --flake ~/.nixdots";
+    git = {
+      enable = true;
+      userName = "Ghx0sty";
+      userEmail = "blobman320@gmail.com";
     };
-  };
 
-  programs.keychain = {
-    enable = true;
-    keys = [ "githubkey" ];  # or your actual key file name
-  };
-
-  # Here goes the Hyprland:
-  wayland.windowManager.hyprland = {
-    enable = true;
-    settings = {
-      "$terminal" = "alacritty";
-      "$launcher-kill" = "pkill ulauncher"; 
-      "$launcher-start" = "ulauncher";
-      "$browser" = "librewolf";
-      
-      "$mainmod" = "ALT";
-      "$secondmod" = "SUPER";
-      monitor = [
-        "Virtual-1,1920x1080@60,0x0,1"
-      ];
-      bind = [
-        "$mainmod, q, exec, $terminal"
-        "$secondmod, b, exec, $browser"
-        "$mainmod, c, killactive"
-        "$mainmod, m, exit"
-
-        "$mainmod, left, movefocus, l"
-        "$mainmod, right, movefocus, r"
-        "$mainmod, up, movefocus, u"
-        "$mainmod, down, movefocus, d"
- 
-        "$secondmod, left, movewindow, l"
-        "$secondmod, right, movewindow, r"
-        "$secondmod, up, movewindow, u"
-        "$secondmod, down, movewindow, d"
-
-        "CTRL ALT, right, workspace, r+1"
-        "CTRL ALT, left, workspace, r-1"
-
-        "$secondmod, 1, workspace, 1"
-        "$secondmod, 2, workspace, 2"
-        "$secondmod, 3, workspace, 3"
-        "$secondmod, 4, workspace, 4"
-        "$secondmod, 5, workspace, 5"
-      ];
-      bindr = [
-        "SUPER, SUPER_L, exec, $launcher-kill || $launcher-start"
-      ];
+    ssh = {
+      enable = true;
+      addKeysToAgent = "yes";
     };
-  };
 
-  programs.alacritty = {
-    enable = true;
-    settings = {
-      font = {
-        normal = {
-          family = "RobotoMono Nerd Font";
-          style = "Regular";
+    zsh = {
+      enable = true;
+      shellAliases = {
+        home-sync = "home-manager switch --flake ~/.nixdots#matt@hackpc";
+        nixos-switch = "sudo nixos-rebuild switch --flake ~/.nixdots#hackpc --use-remote-sudo";
+        nixos-sync = "sudo nixos-rebuild switch --flake ~/.nixdots#hackpc --use-remote-sudo && home-manager switch --flake ~/.nixdots#matt@hackpc";
+        flake-update = "nix flake update --flake ~/.nixdots";
+      };
+    };
+
+    keychain = {
+      enable = true;
+      keys = ["githubkey"]; # or your actual key file name
+    };
+    alacritty = {
+      enable = true;
+      settings = {
+        font = {
+          normal = {
+            family = "RobotoMono Nerd Font";
+            style = "Regular";
+          };
+          bold = {
+            family = "RobotoMono Nerd Font";
+            style = "Bold";
+          };
+          italic = {
+            family = "RobotoMono Nerd Font";
+            style = "Italic";
+          };
+          size = 12.5;
         };
-        bold = {
-          family = "RobotoMono Nerd Font";
-          style = "Bold";
-        };
-        italic = {
-          family = "RobotoMono Nerd Font";
-          style = "Italic";
-        };
-        size = 12.5;
       };
     };
   };
@@ -138,5 +96,4 @@
   systemd.user.startServices = true;
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
-  home.stateVersion = "25.05";
 }
---
+++ home-manager/base/hypr.nix
@@ -0,0 +1,45 @@
+{
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      "$terminal" = "alacritty";
+      "$launcher-kill" = "pkill ulauncher";
+      "$launcher-start" = "ulauncher";
+      "$browser" = "librewolf";
+
+      "$mainmod" = "ALT";
+      "$secondmod" = "SUPER";
+      monitor = [
+        "Virtual-1,1920x1080@60,0x0,1"
+      ];
+      bind = [
+        "$mainmod, q, exec, $terminal"
+        "$secondmod, b, exec, $browser"
+        "$mainmod, c, killactive"
+        "$mainmod, m, exit"
+
+        "$mainmod, left, movefocus, l"
+        "$mainmod, right, movefocus, r"
+        "$mainmod, up, movefocus, u"
+        "$mainmod, down, movefocus, d"
+
+        "$secondmod, left, movewindow, l"
+        "$secondmod, right, movewindow, r"
+        "$secondmod, up, movewindow, u"
+        "$secondmod, down, movewindow, d"
+
+        "CTRL ALT, right, workspace, r+1"
+        "CTRL ALT, left, workspace, r-1"
+
+        "$secondmod, 1, workspace, 1"
+        "$secondmod, 2, workspace, 2"
+        "$secondmod, 3, workspace, 3"
+        "$secondmod, 4, workspace, 4"
+        "$secondmod, 5, workspace, 5"
+      ];
+      bindr = [
+        "SUPER, SUPER_L, exec, $launcher-kill || $launcher-start"
+      ];
+    };
+  };
+}
