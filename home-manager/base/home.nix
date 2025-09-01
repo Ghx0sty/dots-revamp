5fb67b3c72fba7289d3c79df49f4988d92ecc0fd
Ghx0sty <blobman320@gmail.com>
2025-09-01 02:30:43 +0000

    Automated push

--- home-manager/base/home.nix
+++ home-manager/base/home.nix
@@ -22,7 +22,7 @@
 
     # You can also split up your configuration and import pieces of it here:
     # ./nvim.nix
-    ./hypr.nix
+    # ./hypr.nix
   ];
 
   # Nixpkgs configuration moved to flake.nix
