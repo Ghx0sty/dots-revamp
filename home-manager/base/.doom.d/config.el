c7102097ef2956331b5c8378329a780aad21d6ad
Ghx0sty <blobman320@gmail.com>
2025-09-19 09:47:51 +0000

    Automated push

--- home-manager/base/.doom.d/config.el
+++ home-manager/base/.doom.d/config.el
@@ -1,15 +1,1 @@
-(setq doom-theme 'doom-outrun-electric doom-font (font-spec :family "RobotoMono Nerd Font" :weight `medium :size 11.0))
-
-(use-package lsp-mode
-  :ensure t)
-
-(use-package lsp-nix
-  :ensure lsp-mode
-  :after (lsp-mode)
-  :demand t
-  :custom
-  (lsp-nix-nil-formatter ["nixfmt"]))
-
-(use-package nix-mode
-  :hook (nix-mode . lsp-deferred)
-  :ensure t)
+(setq doom-theme 'doom-outrun-electric doom-font (font-spec :family "RobotoMono Nerd Font" :weight `medium :size 12.0))
